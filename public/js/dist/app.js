868cec8a554bdf26f8155eb643dea9b2f8645b8e
Tyl3r035 <31051110+Tyl3r035@users.noreply.github.com>
2022-01-10 08:34:46 -0600

    Add files via upload

---
+++ public/js/dist/app.js
@@ -0,0 +1,18 @@
+let menuBtn = document.getElementsByClassName('menu-btn')[0];
+let mobileMenu = document.getElementsByClassName('mobile-menu')[0];
+let closeBtn = document.getElementsByClassName('mobile-close')[0];
+let mobileLink = document.querySelectorAll('.mobile-nav-link');
+
+menuBtn.addEventListener('click', ()=>{
+    mobileMenu.classList.add('mobile-menu-active')
+})
+menuBtn.classList.remove('menu-btn');
+
+closeBtn.addEventListener('click', ()=>{
+    mobileMenu.classList.remove('mobile-menu-active');
+})
+mobileLink.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        mobileMenu.classList.remove('mobile-menu-active')
+    })
+})
